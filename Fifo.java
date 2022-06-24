a844c5a986ddd40279d7d6827989d81c6f46daf4
gravitybeat <108148460+gravitybeat@users.noreply.github.com>
2022-06-24 19:52:25 +0200

    Add files via upload

---
+++ Fifo.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Fifo extends AbstractIntegerBuffer {
+	
+	Fifo(int s){
+		arr = new Integer[s];
+	}
+	
+	Fifo(){
+		Random r = new Random();
+		int x = r.nextInt(3, 101);
+		arr = new Integer[x];
+	}
+	
+	public Integer pop() {
+		// throws exception wenn stack schon leer ist
+		if (size<1) throw new ArrayIndexOutOfBoundsException();
+		
+		Integer x = arr[0];
+		for(int i=0; i<size; i++) 
+			arr[i] = arr[i+1];
+		
+		size--;
+		arr[size] = 0;
+		return x;
+	}
+	public int front() {
+		if (this.size()==0) throw new NoSuchElementException();
+		return arr[0];
+	}
+}
